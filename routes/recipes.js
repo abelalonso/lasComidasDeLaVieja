f56dad7ffbe363ff8cdf906643f3620479d8361a
Abel Alonso <abel.alonso@gmail.com>
2018-07-17 16:39:58 +0200

    finalizado el post del form de creación de receta

--- routes/recipes.js
+++ routes/recipes.js
@@ -11,9 +11,35 @@ recipeRoutes.get('/add', (req, res, next) => {
 
 
 recipeRoutes.post('/add', upload.single('photo'), (req, res, next) => {
-  console.log(req.user);
-  console.log(req.body);
-
+  const {name, elaborationTime, category, } = req.body;
+  const ingredients = [];
+  const ingredient = req.body.ingredient;
+  const quantity = req.body.quantity;
+  const steps = req.body.step;
+  const keywords = req.body.keyword;
+  for (let i=0; i<ingredient.length; i++){
+    ingredients.push(quantity[i]+' '+ingredient[i]);
+  }
+  const userId = req.user.name;
+  const path = `upload/recipePic/${req.file.filename}`;
+  const originalName = req.file.originalname;
+  newRecipe = new Recipe({
+    name,
+    ingredients,
+    steps,
+    elaborationTime,
+    category,
+    keywords,
+    authorId: req.user.name,
+    recipePic: {path, originalName}
+  });
+  newRecipe.save()
+    .then(()=>{
+      res.redirect("/auth/profile")
+    })
+    .catch((err)=>{
+      res.render('recipes/add', {message: "something went wrong"})
+    })
 });
 
 module.exports = recipeRoutes;
