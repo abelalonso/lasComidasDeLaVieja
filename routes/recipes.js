f995e78e9ee975eb30d18e591e82e96232943781
Pepe Lora <jose.maria.lora.alarcon@gmail.com>
2018-07-17 20:08:38 +0200

    commit malo

--- routes/recipes.js
+++ routes/recipes.js
@@ -11,9 +11,42 @@ recipeRoutes.get('/add', (req, res, next) => {
 
 
 recipeRoutes.post('/add', upload.single('photo'), (req, res, next) => {
-  console.log(req.user);
-  console.log(req.body);
+  const name = req.body.name;
+  const authorId = req.user._id;
+  const ingredients = req.body.ingredient;
+  const steps = req.body.step;
+  const category = req.body.category;
+  const keywords = req.body.keyword;
+  const path = `upload/recipePic/${req.file.filename}`;
+  const originalName = req.file.originalname;
 
-});
+  const newRecipe = new Recipe({
+    name,
+    authorId,
+    ingredients,
+    steps,
+    category,
+    keywords,
+    recipePic: {path, originalName}
+    
+    
+    })
+    console.log(newRecipe)
+   
+    newRecipe.save()
+      .then(rec=>{
+console.log(rec )
+        res.redirect("/auth/profile")
+      })
+      .catch((err)=>{
+        console.log(err)
+        res.render("recipes/new", {message: "An error has ocurred"})
+      })
+      })
+ 
+ 
+
+
+
 
 module.exports = recipeRoutes;
