26c1dc0b9e69b2d150e6dba28df04a6e11d46837
Pepe Lora <jose.maria.lora.alarcon@gmail.com>
2018-07-18 15:38:59 +0200

    index cambiados

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router  = express.Router();
-
+const Recipe = require('../models/Recipe');
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  Recipe.find()
+  .then((recipes)=>{
+    console.log(recipes)
+    res.render('index',{recipes});
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
 });
 
 module.exports = router;
